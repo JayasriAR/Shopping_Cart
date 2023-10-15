b178ac8db49d278e896c5ccde6427ba813dffdc8
Jayasri A R <109610628+JayasriAR@users.noreply.github.com>
2023-10-15 17:54:53 +0530

    Update Script.js

--- assests/js/Script.js
+++ assests/js/Script.js
@@ -7,31 +7,43 @@
 //      }
 // }
 
-function validate(){
+function validate(event){
+  var tit=false;
+  var des=false;
+  var pri=false;
+  var max=false;
+  var sym=false;
+  var fil=false;
   var prodtitle= document.getElementById('pro_title').value;
   var price_symbol=document.getElementById('price_symbol').value;
+
+
   if(price_symbol!='select'){
   localStorage.setItem('price_symbol',price_symbol)
   document.getElementById('price_sym_err').style='display:none;'
-
+  sym=true;
 }
   else{
     document.getElementById('price_sym_err').style='display:block;'
+    sym=false;
   }
 
   if(prodtitle){
         if(prodtitle.length<=3){
         document.getElementById('prodtitle_format_error').style="display: block"
         document.getElementById('prodtitle_error').style="display: none"
+        tit=false;
   }
 else if(prodtitle.length>3){
   document.getElementById('prodtitle_format_error').style="display: none"
   document.getElementById('prodtitle_error').style="display: none"
   localStorage.setItem('prodname',prodtitle)
+  tit=true;
 }}
   else{
     document.getElementById('prodtitle_format_error').style="display: none"
     document.getElementById('prodtitle_error').style="display: block"
+    tit=false;
   }
 
   var proddes=document.getElementById('text_area').value;
@@ -40,17 +52,20 @@ else if(prodtitle.length>3){
   {
     document.getElementById('description_format_err').style="display: block"
         document.getElementById('description_err').style="display: none"
+        des=false;
   }
   else if(proddes.length>=11){
     document.getElementById('description_format_err').style="display:none"
     document.getElementById('description_err').style="display: none"
     localStorage.setItem('proddescription',proddes)
+    des=true;
 
   }
   }
   else{
     document.getElementById('description_format_err').style="display: none"
     document.getElementById('description_err').style="display: block"
+    des=false;
   }
 
 
@@ -61,17 +76,20 @@ if(price){
   if ((!numberPattern.test(price))||(price==0) ){
     document.getElementById('price_format_err').style="display: block"
           document.getElementById('price_err').style="display: none"
+          pri=false;
   }
   else{
     document.getElementById('price_format_err').style="display: none"
           document.getElementById('price_err').style="display: none"
       localStorage.setItem('price',price)
+      pri=true;
   
   }
   }
   else{
     document.getElementById('price_format_err').style="display: none"
           document.getElementById('price_err').style="display: block"
+          pri=false;
   }
   
   
@@ -80,24 +98,44 @@ if(maxlimit){
 if (!numberPattern.test(maxlimit)) {
   document.getElementById('maxlimit_format_err').style="display: block"
         document.getElementById('maxlimit_err').style="display: none"
+        max=false;
 }
 else{
   document.getElementById('maxlimit_format_err').style="display: none"
         document.getElementById('maxlimit_err').style="display: none"
     localStorage.setItem('prod_limit',maxlimit)
-
+    max=true;
 }
 }
 else{
   document.getElementById('maxlimit_format_err').style="display: none"
         document.getElementById('maxlimit_err').style="display: block"
-}
+        max=false;
 }
 
+var inputElement = document.getElementById("input_image");
+if (inputElement.files.length > 0) {
+  fil=true;
+  document.getElementById('validimg_err2').style="display: none"
+} else {
+  fil=false;
+  document.getElementById('validimg_err2').style="display: block"
+}
 
+const redirectButton = document.getElementById('redirect');
+
+    redirectButton.addEventListener('click', function () {
+            if (tit==true && fil==true && des==true && pri==true && max==true && sym==true) {
+                // If the condition is satisfied, redirect to a specific page
+                window.location.href = './assests/files/product.html'; // Replace with your desired URL
+            }
+          
+        });
+
+}
 
 function preview_image(event){
-console.log('------event------',event);
+  
 let extension=event.target.files[0].type.split('/');
 var exten=extension[1];
 if(exten=='jpg' || exten=='png' ||exten=='jpeg'){
@@ -120,6 +158,6 @@ reader.readAsDataURL(file_name);
 else{
     document.getElementById('validimg_err').style="display:block"
 }
-}
 
+}
 
